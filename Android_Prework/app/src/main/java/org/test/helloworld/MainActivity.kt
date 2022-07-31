d8ca9651d0381dd4b0d6da9773551186dd444257
ResidentTenko <109637465+ResidentTenko@users.noreply.github.com>
2022-07-30 23:03:41 -0400

    Prework Added

---
+++ Android_Prework/app/src/main/java/org/test/helloworld/MainActivity.kt
@@ -0,0 +1,22 @@
+package org.test.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.helloButton)
+        button.setOnClickListener {
+            Log.v("Hello world", "Button clicked!")
+            Toast.makeText(this, getString(R.string.response_hello), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
